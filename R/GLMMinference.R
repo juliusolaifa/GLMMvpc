59c2739109622093519ade7a823f23fd3d7772f2
juliusolaifa <jbolaifa@gmail.com>
2023-11-22 20:59:30 -0600

    likelihood ratio test updated

--- R/GLMMinference.R
+++ R/GLMMinference.R
@@ -1,23 +1,39 @@
-vpc.lrt <- function(vpcObj, nullfixed, nullrandom) {
-  logLik1 <- vpcObj$logLikelihood
-  m0 <- GLMMadaptive::mixed_model(fixed = nullfixed, random = nullrandom,
-                                       data = vpcObj$modObj$data, 
-                                       family = vpcObj$modObj$family)
-  logLik0 <- m0$logLik
-  lambda <- -2*(logLik0 - logLik1)
-  pval <- p_value()
+lrtest <- function(x, ...) {
+  UseMethod("lrtest")
+}
 
-  p_value <- function(bou_in, nob_in, nob_nu) {
-    if(bou_in == 1 && nob_in == 0 && nob_nu == 2) {
-      0.5*stats::pchisq(lambda, df = 1, lower.tail = FALSE) + 
+
+lrtest.vpcscore <- function(vpcObj, H0fixed, H0random = NULL, alpha = 0.05) {
+  logLik1 <- vpcObj$logLikelihood
+  
+  if (is.null(H0random)) {
+    family <- match_family(vpcObj$modObj$family$family)
+    m0 <- suppressWarnings(glmmTMB::glmmTMB(formula = H0fixed, family = family, 
+              data = vpcObj$modObj$data))
+    m0$nvarcomp_bounded <- 0
+    m0$nfixed <- length(m0$obj$par)
+ 
+  } else{
+    m0 <- glmm.fit(fixed = H0fixed, random = H0random,
+                                    data = vpcObj$modObj$data, 
+                                    family = vpcObj$modObj$family)
+  }
+  
+  logLik0 <- as.numeric(stats::logLik(m0))
+  lambda <- -2*(logLik0 - logLik1)
+
+    if(is.null(H0random) && vpcObj$nvarcomp_bounded == 1) {
+      pval <- 0.5*stats::pchisq(lambda, df = 1, lower.tail = FALSE) + 
         0.5*(lambda == 0)
-    }else if(bou_in == 1 && nob_in == 2 && nob_nu == 3) {
-      0.5*stats::pchisq(lambda, df = 1, lower.tail = FALSE) +
+    }else if(vpcObj$nvarcomp_bounded == 2 && m0$nvarcomp_bounded == 1) {
+      if(vpcObj$nfixef == m0$nfixef){
+        pval <- 0.5*stats::pchisq(lambda, df = 1, lower.tail = FALSE) +
         0.5*stats::pchisq(lambda, df = 2, lower.tail = FALSE)
-    }else if(bou_in == 1 && nob_in == 3 && nob_nu == 2) {
-      0.5*stats::pchisq(lambda, df = 1, lower.tail = FALSE) +
-        0.5*stats::pchisq(lambda, df = 2, lower.tail = FALSE)
-    }else if(bou_in == 2 && nob_in == 1 && nob_nu == 3) {
+      }else if(vpcObj$nfixef == m0$nfixef + 1) {
+        pval <- 0.5*stats::pchisq(lambda, df = 2, lower.tail = FALSE) +
+        0.5*stats::pchisq(lambda, df = 3, lower.tail = FALSE)
+      }
+    }else if(is.null(H0random) && vpcObj$nvarcomp_bounded == 2) {
       V <- stats::vcov(vpcObj$modObj)
       vbvar <- matrix(c(V[3,3],V[3,5],
                         V[5,3],V[5,5]),
@@ -25,11 +41,38 @@ vpc.lrt <- function(vpcObj, nullfixed, nullrandom) {
       pi1 <- as.numeric(mvtnorm::pmvnorm(lower = c(0,0), upper = c(Inf,Inf),
                                          mean = c(0,0), sigma = vbvar))
       pi4 <- 0.5 - pi1
-      pi1*stats::pchisq(lambda, df = 1, lower.tail = FALSE) +
+      pval <- pi1*stats::pchisq(lambda, df = 1, lower.tail = FALSE) +
         0.5*stats::pchisq(lambda, df = 2, lower.tail = FALSE) +
         pi4*stats::pchisq(lambda, df = 3, lower.tail = FALSE) 
-    }else{
-      stop("`vpc.lrt` not implemented for this case")
     }
+    result <- list("lambda" = lambda, "p_value" = pval, "alpha" = alpha)
+    class(result) <- "glmmlrtest"
+    return(result)
+}
+
+print.glmmlrtest <- function(testObj) {
+  cat("Likelihood Ratio Test\n")
+  cat("---------------------\n")
+  cat("Lambda: ")
+  cat(testObj$lambda)
+  cat("\n")
+  cat("P-value: ")
+  cat(testObj$p_value)
+  cat("\n")
+  if(testObj$p_value < testObj$alpha)
+    cat("Reject the Null Hypothesis")
+  else
+    cat("Do not reject the Null hypothesis")
+}
+
+match_family <- function(fam) {
+  adaptive_fam <- c("negative binomial")
+  glmmTMB_fam <- c("nbinom2")
+  index <- match(fam, adaptive_fam)
+  if (!is.na(index)) {
+    return(glmmTMB_fam[index])
+  } else {
+    return(NA)
   }
 }
+
